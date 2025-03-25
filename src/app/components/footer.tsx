1cd56e85e61509173846e2b8711a92eb49607603
edwardfar <ryan@moonstarfoodus.com>
2025-03-25 00:49:50 -0400

    Remove footer from layout and update header

--- src/app/components/footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white p-4 text-center">
-        © 2025 MoonStar Food LLC. All rights reserved.
-      </footer>
-    );
-  }
-  
